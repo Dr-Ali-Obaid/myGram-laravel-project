fa0b011bc0c067d2d1323b6fab785309a546be3b
dr-ali-obaid <dr.ali.emam@gmail.com>
2025-04-11 01:05:28 +0300

    try 6

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -20,7 +20,7 @@
         <div class="sm:col-span-4">
           <label for="about" class="block text-sm/6 font-medium text-gray-900">{{__("Bio")}}</label>
           <div class="mt-2">
-            <textarea name="about" id="about" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">{{$user->bio}}</textarea>
+            <textarea name="bio" id="about" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">{{$user->bio}}</textarea>
           </div>
         </div>
 
